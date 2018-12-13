03931317a1ecb17e857c74324b5e3fa1eb704b60
David Pineda <davidpineda1003@gmail.com>
2018-12-13 00:05:27 -0500

    Agregando Apollo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  uri: "https://w5xlvm3vzz.lp.gql.zone/graphql"
+});
+
+const MyApp = () =>
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+
+ReactDOM.render(<MyApp />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
