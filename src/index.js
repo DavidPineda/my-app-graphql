2be2a144955e4424cc2661508fb7dad909468644
David Pineda <davidpineda1003@gmail.com>
2018-12-13 23:35:07 -0500

    Mutaciones

--- src/index.js
+++ src/index.js
@@ -3,19 +3,25 @@ import ReactDOM from 'react-dom';
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import Create from './Create';
+import Update from './Update'
+
 const client = new ApolloClient({
-  uri: "https://w5xlvm3vzz.lp.gql.zone/graphql"
+  uri: "https://8v9r9kpn7q.lp.gql.zone/graphql"
 });
 
-const MyApp = () =>
+const App = () =>
   <ApolloProvider client={client}>
-    <App />
+    <div>
+      <h2>Contruyendo componentes mutables <span role="img" aria-label="Hola">🚀</span></h2>
+      <Create />
+      <Update />
+    </div>
   </ApolloProvider>
 
-ReactDOM.render(<MyApp />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
